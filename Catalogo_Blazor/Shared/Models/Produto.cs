bbc2558e83181887617e565b648f1c26d3eddf61
Humberto Marcone <hmarcone@gmail.com>
2021-03-04 19:32:27 -0300

    Add project files.

---
+++ Catalogo_Blazor/Shared/Models/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Catalogo.Shared.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do produto")]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição do produto")]
+        [MaxLength(200)]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        public decimal Preco { get; set; }
+
+        [MaxLength(250)]
+        public string ImagemUrl { get; set; }
+
+        //indica o relacionamento
+        public int CategoriaId { get; set; }
+
+        //propriedade de navegação
+        public virtual Categoria Categoria { get; set; }
+    }
+}
