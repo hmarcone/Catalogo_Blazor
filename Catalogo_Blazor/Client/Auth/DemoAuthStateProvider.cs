84538d18a93d073cecdc91daca92edf0c9fa5960
Humberto Marcone <hmarcone@gmail.com>
2021-03-16 19:05:29 -0300

    Implementando a segurança via autenticação.

---
+++ Catalogo_Blazor/Client/Auth/DemoAuthStateProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Blazor_Catalogo.Client.Auth
+{
+    public class DemoAuthStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState>
+            GetAuthenticationStateAsync()
+        {
+            await Task.Delay(2000);
+            //indicamos se o usuário esta autenticado e 
+            //também os seus claims
+
+            var usuario = new ClaimsIdentity(new List<Claim>() {
+              new Claim("Chave", "Valor"),
+              new Claim(ClaimTypes.Name, "Humberto Marcone"),
+              new Claim(ClaimTypes.Role, "Admin")
+            }, "demo");
+
+            //var usuario = new ClaimsIdentity();
+
+            return await Task.FromResult(new AuthenticationState(
+                new ClaimsPrincipal(usuario)));
+        }
+    }
+}
