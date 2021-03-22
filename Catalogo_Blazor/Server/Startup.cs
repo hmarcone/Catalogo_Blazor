d0d3afddbbbd6c73221ab524a9f8be5070d7a926
Humberto Marcone <hmarcone@gmail.com>
2021-03-22 16:01:26 -0300

    Implementando o controlador AccountController

--- Catalogo_Blazor/Server/Startup.cs
+++ Catalogo_Blazor/Server/Startup.cs
@@ -74,6 +74,9 @@ namespace Catalogo_Blazor.Server
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
---
+++ Catalogo_Blazor/Shared/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Catalogo.Shared.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
